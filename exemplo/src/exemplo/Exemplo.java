92b110e573d6f5613a902fc2ad2947b427558d6c
samusafe <samucarvalhocp@gmail.com>
2022-10-20 14:15:02 +0100

    first lesson

---
+++ exemplo/src/exemplo/Exemplo.java
@@ -0,0 +1,28 @@
+package exemplo;
+
+public class Exemplo {
+	
+	public static void main(String[] args) {
+		
+		int[] list;
+		list = new int[10];
+		list[0] = 1;
+		
+		System.out.println("ola");
+		System.out.println(list[0]);
+		
+		long value1 = 0;
+		System.out.println(value1 + " " + list[0]);
+		
+		value1 = 3;
+		System.out.println(value1);
+		
+		boolean exercise = false;
+		System.out.println(exercise);
+		
+		double value2;
+		int value3;
+		//System.out.println(value2);
+		//System.out.println(value3);
+	}
+}
